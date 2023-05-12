77b92eff7d1641ef1a63ce620cf05c425f475611
Sahil <interactwithsahil@gmail.com>
2023-05-12 14:40:49 +0530

    Initial commit

---
+++ 6 Projects on Event Listener/DoubleClick/doubleClick.js
@@ -0,0 +1,14 @@
+// Using onClick Event listener
+
+//Getting the HTML Elements
+
+const messageButton = document.getElementById("messageBtn");
+
+// Message
+messageButton.addEventListener("dblclick", () => {
+    const message = document.createElement("p");
+    message.textContent = "Endure to the End";          //Message to be Displayed
+    message.style.color = "red";
+    message.style.fontsize = "25px";
+    document.body.appendChild(message);
+});
